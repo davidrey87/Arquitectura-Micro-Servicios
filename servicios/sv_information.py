cb0c10ebbd55d0287d2ad690319f1f592a0f5add
Alexander Padilla <yamop.16@gmail.com>
2017-10-25 17:44:21 -0500

    update api

--- servicios/sv_information.py
+++ servicios/sv_information.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
-#----------------------------------------------------------------------------------------------------------------
+# !/usr/bin/env python
+# ----------------------------------------------------------------------------------------------------------------
 # Archivo: sv_information.py
 # Tarea: 2 Arquitecturas Micro Servicios.
 # Autor(es): Perla Velasco & Yonathan Mtz.
-# Version: 1.2 Abril 2017
+# Version: 1.3 Octubre 2017
 # Descripción:
 #
 #   Este archivo define el rol de un servicio. Su función general es porporcionar en un objeto JSON
 #   información detallada acerca de una pelicula o una serie en particular haciendo uso del API proporcionada
 #   por IMDb ('https://www.imdb.com/').
-#   
-#   
+#
+#
 #
 #                                        sv_information.py
 #           +-----------------------+-------------------------+------------------------+
@@ -29,31 +29,35 @@
 import os
 from flask import Flask, abort, render_template, request
 import urllib, json
-app = Flask (__name__)
+
+app = Flask(__name__)
+
 
 @app.route("/api/v1/information")
 def get_information():
-	# Método que obtiene la información de IMDB acerca de un título en particular
-	# Se lee el parámetro 't' que contiene el título de la película o serie que se va a consultar
-	title = request.args.get("t")
-	# Se verifica si el parámetro no esta vacío 
-	if title is not None:
-		# Se conecta con el servicio de IMDb a través de su API
-		url_omdb = urllib.urlopen("http://www.omdbapi.com/?t="+title+"&plot=full&r=json")
-		# Se lee la respuesta de IMDb
-		json_omdb = url_omdb.read()
-		# Se convierte en un JSON la respuesta recibida
-		omdb = json.loads(json_omdb)
-		# Se regresa el JSON de la respuesta
-		return json.dumps(omdb)
-	else:
-		# Se devuelve un error 400 para indicar que el servicio no puede funcionar sin parámetro
-		abort(400)
+    """
+    Este método obtiene información acerca de una película o serie
+    específica.
+    :return: JSON con la información de la película o serie
+    """
+    # Se lee el parámetro 't' que contiene el título de la película o serie que se va a consultar
+    title = request.args.get("t")
+    api_key = '7aed8d98'
+    url_base = 'http://www.omdbapi.com/?apikey=' + api_key + '&t='
+    # Se conecta con el servicio de IMDb a través de su API
+    url_omdb = urllib.urlopen(url_base + title + "&plot=full&r=json")
+    # Se lee la respuesta de IMDb
+    json_omdb = url_omdb.read()
+    # Se convierte en un JSON la respuesta recibida
+    omdb = json.loads(json_omdb)
+    # Se regresa el JSON de la respuesta
+    return json.dumps(omdb), 200
+
 
 if __name__ == '__main__':
-	# Se define el puerto del sistema operativo que utilizará el servicio
-	port = int(os.environ.get('PORT', 8084))
-	# Se habilita la opción de 'debug' para visualizar los errores
-	app.debug = True
-	# Se ejecuta el servicio definiendo el host '0.0.0.0' para que se pueda acceder desde cualquier IP
-	app.run(host='0.0.0.0', port=port)
+    # Se define el puerto del sistema operativo que utilizará el servicio
+    port = int(os.environ.get('PORT', 8084))
+    # Se habilita la opción de 'debug' para visualizar los errores
+    app.debug = True
+    # Se ejecuta el servicio definiendo el host '0.0.0.0' para que se pueda acceder desde cualquier IP
+    app.run(host='0.0.0.0', port=port)
